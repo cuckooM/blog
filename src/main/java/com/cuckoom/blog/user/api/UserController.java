327697d637ca21c416f6ce3298f04608e963c456
cuckooM <meng891216@163.com>
2023-04-14 13:03:13 +0800

    修改 Token 结构

---
+++ src/main/java/com/cuckoom/blog/user/api/UserController.java
@@ -0,0 +1,41 @@
+package com.cuckoom.blog.user.api;
+
+import java.security.Principal;
+import java.util.List;
+
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.cuckoom.blog.label.dto.LabelDTO;
+import com.cuckoom.blog.security.SecurityUtils;
+import com.cuckoom.blog.user.dto.UserDTO;
+import com.cuckoom.blog.user.service.UserService;
+
+import lombok.RequiredArgsConstructor;
+
+/**
+ * 用户 API 控制器
+ * @author cuckooM
+ */
+@RestController
+@RequestMapping("/api/user")
+@RequiredArgsConstructor
+public class UserController {
+
+    /** 用户业务逻辑层接口 */
+    private final UserService userService;
+
+    /**
+     * 查询当前用户
+     * @param principal 当前用户信息
+     * @return 结果
+     */
+    @GetMapping("/current")
+    public ResponseEntity<UserDTO> current(Principal principal) {
+        return ResponseEntity.ok(userService.findById(SecurityUtils.getUserId(principal)));
+    }
+
+}
